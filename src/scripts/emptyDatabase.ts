eed46074b750034d542c09eaf090907a5c6fea7d
David Szankovszky <szankovszky.david@gmail.com>
2025-03-20 17:02:34 +0100

    import.meta.dirname replaced with import.meta.url in generateFakeDatabaseData.ts and emptyDatabase.ts

--- src/scripts/emptyDatabase.ts
+++ src/scripts/emptyDatabase.ts
@@ -1,6 +1,6 @@
-import { join } from "path";
+import { dirname, join } from "path";
 
-const __dirname = import.meta.dirname;
+const __dirname = dirname(new URL(import.meta.url).pathname);
 const authorModelPath = join(__dirname, "../logger/model/author.model.js");
 const dbFilePath = join(__dirname, "../logger/database/database.js");
 
--- src/scripts/generateFakeDatabaseData.ts
+++ src/scripts/generateFakeDatabaseData.ts
@@ -1,8 +1,8 @@
-import { join } from "path";
+import { dirname, join } from "path";
 
-const __dirname = import.meta.dirname;
+const __dirname = dirname(new URL(import.meta.url).pathname);
 const fakerPath = join(__dirname, "../logger/database/faker/dataFaker.js");
 
 const { runFaker } = await import(`file://${fakerPath}`);
-console.log(`file://${fakerPath}`);
+
 await runFaker();
