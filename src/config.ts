c42a98fab2796afdd31dd99081b61d41adf710cc
David Szankovszky <szankovszky.david@gmail.com>
2025-03-25 18:05:09 +0100

    Husky added to the project

--- src/config.ts
+++ src/config.ts
@@ -6,8 +6,8 @@ dotenv.config();
 const { DISCORD_TOKEN, DISCORD_CLIENT_ID, GUILD_ID, DB_PATH } = process.env;
 
 if (!DISCORD_TOKEN || !DISCORD_CLIENT_ID || !GUILD_ID) {
-  logger.error("Missing environment variables");
-  throw new Error("Missing environment variables");
+  logger.error('Missing environment variables');
+  throw new Error('Missing environment variables');
 }
 
 export const config = {
