cc93998cae4c7c62cf6708ff13eb43d74cfa13ff
David Szankovszky <szankovszky.david@gmail.com>
2025-03-25 18:08:13 +0100

    DISCORD_TOKEN_DEV replaced with DISCORD_TOKEN in deploy-commands.ts

--- src/bot/client/deploy-commands.ts
+++ src/bot/client/deploy-commands.ts
@@ -5,7 +5,7 @@ import { logger } from '../../winston/winston.js';
 
 const commandsData = Object.values(commands).map((command) => command.data);
 
-const rest = new REST({ version: '10' }).setToken(config.DISCORD_TOKEN_DEV);
+const rest = new REST({ version: '10' }).setToken(config.DISCORD_TOKEN);
 
 export async function deployCommands() {
   try {
