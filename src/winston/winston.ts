d48ecd1a5e3b8f05c20540d1bb70d51cf9f19b34
David Szankovszky <szankovszky.david@gmail.com>
2025-03-31 14:13:16 +0200

    Loki log levels removed

--- src/winston/winston.ts
+++ src/winston/winston.ts
@@ -10,16 +10,7 @@ const __dirname = path.dirname(__filename);
 const logsDirectory = path.resolve(__dirname, '../logs');
 
 const errorFilter = winston.format((info) => info.level === 'error' ? info : false);
-
-const warnFilter = winston.format((info) => info.level === 'warn' ? info : false);
-
 const infoFilter = winston.format((info) => info.level === 'info' ? info : false);
-
-const httpFilter = winston.format((info) => info.level === 'http' ? info : false);
-const verboseFilter = winston.format((info) => info.level === 'verbose' ? info : false);
-
-const debugFilter = winston.format((info) => info.level === 'debug' ? info : false);
-const sillyFilter = winston.format((info) => info.level === 'silly' ? info : false);
 
 export const logger = winston.createLogger({
   level: process.env.LOG_LEVEL_DEV || 'silly',
@@ -30,13 +21,12 @@ export const logger = winston.createLogger({
   ),
   transports: [
     new LokiTransport({
-        host: 'http://loki:3100',
-        labels: { app: 'sven' },
-        json: true,
-        format: winston.format.json(),
-        replaceTimestamp: true,
-        // eslint-disable-next-line no-console
-        onConnectionError: (err) => console.error(err),
+      host: 'http://loki:3100',
+      labels: { app: 'sven' },
+      json: true,
+      format: winston.format.json(),
+      replaceTimestamp: true,
+      onConnectionError: (err) => logger.error(err),
     }),
     new winston.transports.DailyRotateFile({
       filename: path.join(logsDirectory, 'combined-%DATE%.log'),
@@ -51,46 +41,11 @@ export const logger = winston.createLogger({
       format: combine(errorFilter(), timestamp(), json()),
     }),
     new winston.transports.DailyRotateFile({
-      filename: path.join(logsDirectory, 'app-warn-%DATE%.log'),
-      level: 'warn',
-      datePattern: 'YYYY-MM-DD',
-      maxFiles: '14d',
-      format: combine(warnFilter(), timestamp(), json()),
-    }),
-    new winston.transports.DailyRotateFile({
       filename: path.join(logsDirectory, 'app-info-%DATE%.log'),
       level: 'info',
       datePattern: 'YYYY-MM-DD',
       maxFiles: '14d',
       format: combine(infoFilter(), timestamp(), json()),
-    }),
-    new winston.transports.DailyRotateFile({
-      filename: path.join(logsDirectory, 'app-http-%DATE%.log'),
-      level: 'http',
-      datePattern: 'YYYY-MM-DD',
-      maxFiles: '14d',
-      format: combine(httpFilter(), timestamp(), json()),
-    }),
-    new winston.transports.DailyRotateFile({
-      filename: path.join(logsDirectory, 'app-verbose-%DATE%.log'),
-      level: 'verbose',
-      datePattern: 'YYYY-MM-DD',
-      maxFiles: '14d',
-      format: combine(verboseFilter(), timestamp(), json()),
-    }),
-    new winston.transports.DailyRotateFile({
-      filename: path.join(logsDirectory, 'app-debug-%DATE%.log'),
-      level: 'debug',
-      datePattern: 'YYYY-MM-DD',
-      maxFiles: '14d',
-      format: combine(debugFilter(), timestamp(), json()),
-    }),
-    new winston.transports.DailyRotateFile({
-      filename: path.join(logsDirectory, 'app-silly-%DATE%.log'),
-      level: 'silly',
-      datePattern: 'YYYY-MM-DD',
-      maxFiles: '14d',
-      format: combine(sillyFilter(), timestamp(), json()),
     }),
   ],
   exceptionHandlers: [
