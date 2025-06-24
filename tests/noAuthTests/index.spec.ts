d772b18ab53c48cd6463f35a92d3ca0c1a377f4b
David Szankovszky <szankovszky.david@gmail.com>
2025-06-24 14:08:07 +0200

    playwright test links replaced

--- tests/noAuthTests/index.spec.ts
+++ tests/noAuthTests/index.spec.ts
@@ -8,36 +8,45 @@ test.describe('Root Page', () => {
     });
 
     test('should display the main heading and paragraph correctly', async ({ page }) => {
-        await expect(page).toHaveTitle(/Discord Server Monitoring/);
+        await expect(page).toHaveTitle('Discord Server Monitoring');
 
-        const header = page.getByRole('heading', { name: 'Know Your Server', level: 1 });
-        await expect(header).toBeVisible();
-        await expect(header).toHaveClass('text-base md:text-2xl font-bold');
+        const navbar = page.getByRole('navigation');
 
-        const paragraph = page.getByText('Monitor, Collect Statistics, Make Conclusions');
-        await expect(paragraph).toBeVisible();
-        await expect(paragraph).toHaveClass('text-sm text-gray-600');
+        const navbarText = navbar.locator('div[class="mb-8 text-2xl font-bold"]');
+        await expect(navbarText).toBeVisible();
+        await expect(navbarText).toHaveText('Server Logs');
+        await expect(navbarText).toHaveClass('mb-8 text-2xl font-bold');
 
-        const mainLoginAnchor = page.getByTestId('mainLoginButton');
-        await expect(mainLoginAnchor).toBeVisible();
-        await expect(mainLoginAnchor).toHaveClass('mt-6 inline-block py-2 px-6 rounded-lg bg-gray-700 hover:bg-gray-600 text-white border border-white transition duration-300 ease-in-out');
+        const navbarLogin = navbar.locator('a[href="https://discord.com/oauth2/authorize?client_id=1352271959232086026&response_type=code&redirect_uri=https%3A%2F%2Fsvenbot.cloud%2Fapi%2Flogin&scope=guilds+identify"]');
+        await expect(navbarLogin).toBeVisible();
+
+        const rootHeader = page.getByRole('heading', { name: 'Know Your Server', level: 1 });
+        await expect(rootHeader).toBeVisible();
+        await expect(rootHeader).toHaveClass('text-base md:text-2xl font-bold');
+
+        const rootParagraph = page.getByText('Monitor, Collect Statistics, Make Conclusions');
+        await expect(rootParagraph).toBeVisible();
+        await expect(rootParagraph).toHaveClass('text-sm text-gray-600');
+
+        const rootLogin = page.locator('div[class="text-center space-y-4"] > a[href="https://discord.com/oauth2/authorize?client_id=1352271959232086026&response_type=code&redirect_uri=https%3A%2F%2Fsvenbot.cloud%2Fapi%2Flogin&scope=guilds+identify"]');
+        await expect(rootLogin).toBeVisible();
+        await expect(rootLogin).toHaveText('Login');
     });
 
-    test('should navigate to the /login page when Login button is clicked in main section', async ({ page }) => {
-        const loginAnchor = page.getByTestId('mainLoginButton');
-        await expect(loginAnchor).toBeVisible();
+    test('should route to the /login page if Login button is clicked on either in the nav sidebar or in the page', async ({ page }) => {
+        const navbar = page.getByRole('navigation');;
 
-        await loginAnchor.click();
+        const navbarLogin = navbar.locator('a[href="https://discord.com/oauth2/authorize?client_id=1352271959232086026&response_type=code&redirect_uri=https%3A%2F%2Fsvenbot.cloud%2Fapi%2Flogin&scope=guilds+identify"]');
+        await expect(navbarLogin).toBeVisible();
+        await navbarLogin.click();
+        await expect(page).toHaveURL('https://discord.com/oauth2/authorize?client_id=1352271959232086026&response_type=code&redirect_uri=https%3A%2F%2Fsvenbot.cloud%2Fapi%2Flogin&scope=guilds+identify');
 
-        await expect(page).toHaveURL('https://discord.com/oauth2/authorize?client_id=1352273717623001209&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Flogin&scope=guilds+identify');
-    });
+        await page.goto(`${BASE_URL}/`);
 
-    test('should navigate to the /login page when Login button is clicked in the navbar', async ({ page }) => {
-        const loginAnchor = page.getByTestId('navbarLoginButton');
-        await expect(loginAnchor).toBeVisible();
-
-        await loginAnchor.click();
-
-        await expect(page).toHaveURL('https://discord.com/oauth2/authorize?client_id=1352273717623001209&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Flogin&scope=guilds+identify');
+        const rootLogin = page.locator('div[class="text-center space-y-4"] > a[href="https://discord.com/oauth2/authorize?client_id=1352271959232086026&response_type=code&redirect_uri=https%3A%2F%2Fsvenbot.cloud%2Fapi%2Flogin&scope=guilds+identify"]');
+        await expect(rootLogin).toBeVisible();
+        await expect(rootLogin).toHaveText('Login');
+        await rootLogin.click();
+        await expect(page).toHaveURL('https://discord.com/oauth2/authorize?client_id=1352271959232086026&response_type=code&redirect_uri=https%3A%2F%2Fsvenbot.cloud%2Fapi%2Flogin&scope=guilds+identify');
     });
 });
